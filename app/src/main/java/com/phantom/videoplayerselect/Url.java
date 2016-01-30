f20f8740f054e5429b2ffed973818995b1a75a55
Ashish Bansal <bansal.ashish096@gmail.com>
2016-01-30 16:44:04 +0530

    Added URL compare helper functions

--- app/src/main/java/com/phantom/videoplayerselect/Url.java
+++ app/src/main/java/com/phantom/videoplayerselect/Url.java
@@ -1,5 +1,10 @@
 package com.phantom.videoplayerselect;
 
+import android.util.Log;
+
+import java.text.DateFormat;
+import java.util.Date;
+
 public class Url {
     private String mUrl, mMetadata;
 
@@ -14,5 +19,30 @@ public class Url {
 
     public String getMetadata() {
         return mMetadata;
+    }
+
+    @Override
+    public boolean equals(Object urlObj) {
+        if (urlObj == null) {
+            return false;
+        }
+
+        Url otherUrl = (Url) urlObj;
+        if (mUrl.equals(otherUrl.getUrl()) && mMetadata.equals(otherUrl.getMetadata())) {
+            return true;
+        }
+        return false;
+    }
+
+    public int compareTo(Url urlObj) {
+        Url url = (Url)urlObj;
+        try {
+            Date thisDate = DateFormat.getDateTimeInstance().parse(mMetadata);
+            Date urlObjDate = DateFormat.getDateTimeInstance().parse(url.getMetadata());
+            return thisDate.compareTo(urlObjDate);
+        } catch(java.text.ParseException e) {
+            Log.e("URL", e.getStackTrace().toString());
+        }
+        return 0;
     }
 }
