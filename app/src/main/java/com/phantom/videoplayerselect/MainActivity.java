e6a1f48021355a1ca6daa9474039c58527b105eb
Ashish Bansal <bansal.ashish096@gmail.com>
2016-01-30 23:50:57 +0530

    Moved MainAcitivity to UrlLog; Set MainActivity as preference fragment

--- app/src/main/java/com/phantom/videoplayerselect/MainActivity.java
+++ app/src/main/java/com/phantom/videoplayerselect/MainActivity.java
@@ -1,94 +1,26 @@
 package com.phantom.videoplayerselect;
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-import co.dift.ui.SwipeToAction;
-
 public class MainActivity extends AppCompatActivity {
-    private UrlList mUrlList;
     private Toolbar toolbar;
-    private SwipeRefreshLayout mSwipeRefreshLayout;
-    RecyclerView recyclerView;
-    UrlAdapter adapter;
-    SwipeToAction swipeToAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mUrlList = UrlList.getUrlListSingleton(this);
-
-        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
-        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                mUrlList.clearLocalList();
-                mUrlList.loadSavedUrls();
-                displaySnackbar("URL List updated", null, null);
-                mSwipeRefreshLayout.setRefreshing(false);
-            }
-        });
-
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        recyclerView = (RecyclerView) findViewById(R.id.recycler);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setHasFixedSize(true);
-
-        adapter = new UrlAdapter(mUrlList);
-        recyclerView.setAdapter(adapter);
-
-        swipeToAction = new SwipeToAction(recyclerView, new SwipeToAction.SwipeListener<Url>() {
-            @Override
-            public boolean swipeLeft(final Url url) {
-                removeUrl(url);
-                displaySnackbar(url.getUrl() + " removed", "Undo", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        addUrl(url);
-                        adapter.notifyDataSetChanged();
-                    }
-                });
-                return true;
-            }
-
-            @Override
-            public boolean swipeRight(Url itemData) {
-                ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-                Uri copyUri = Uri.parse(itemData.getUrl());
-                ClipData clip = ClipData.newUri(getContentResolver(), "URI", copyUri);
-                clipboard.setPrimaryClip(clip);
-                displaySnackbar("URL copied", null, null);
-                return true;
-            }
-
-            @Override
-            public void onClick(Url itemData) {
-                displaySnackbar("Filename : " + itemData.getFilename(), null, null);
-            }
-
-            @Override
-            public void onLongClick(Url itemData) {
-            }
-        });
+        getFragmentManager().beginTransaction().replace(R.id.content_frame, new CustomPreferenceFragment()).commit();
     }
 
     private void displaySnackbar(String text, String actionName, View.OnClickListener action) {
@@ -103,17 +35,6 @@ public class MainActivity extends AppCompatActivity {
         snack.show();
     }
 
-    private int removeUrl(Url url) {
-        int position = UrlList.getUrlListSingleton(this).removeUrl(url);
-        adapter.notifyItemRemoved(position);
-        return position;
-    }
-
-    private void addUrl(Url url) {
-        mUrlList.addUrl(url);
-        adapter.notifyDataSetChanged();
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -125,9 +46,6 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_clear_urls) {
-            mUrlList.clearLocalList();
-            mUrlList.clearSavedList();
-            adapter.notifyDataSetChanged();
             displaySnackbar("URL List cleared", null, null);
             return true;
         }
