fab1405f17ce8a353d5cd985d02f52fba0c75ad8
Ashish Bansal <bansal.ashish096@gmail.com>
2016-01-30 17:24:05 +0530

    Indented Code

--- app/src/main/java/com/phantom/videoplayerselect/UrlList.java
+++ app/src/main/java/com/phantom/videoplayerselect/UrlList.java
@@ -8,7 +8,9 @@ import java.util.Collections;
 import java.util.Vector;
 
 public class UrlList {
-    private UrlList() {}
+    private UrlList() {
+    }
+
     public static final String PREFS_NAME = "SavedUrls";
     public static UrlList mUrlList;
     Vector<Url> mList = new Vector<>();
@@ -34,17 +36,17 @@ public class UrlList {
     public void addUrl(Url url) {
         SharedPreferences settings = mContext.getSharedPreferences(PREFS_NAME, 0);
         int urlSavedCount = settings.getInt("count", 0);
-        for(int i = 0; i < urlSavedCount*2; i+=2) {
-            String savedUrl = settings.getString(Integer.toString(i+1), "");
-            String savedMetadata = settings.getString(Integer.toString(i+2), "");
+        for (int i = 0; i < urlSavedCount * 2; i += 2) {
+            String savedUrl = settings.getString(Integer.toString(i + 1), "");
+            String savedMetadata = settings.getString(Integer.toString(i + 2), "");
             if (savedUrl.equals(url.getUrl()) && savedMetadata.equals(url.getMetadata())) {
                 return;
             }
         }
 
         SharedPreferences.Editor editor = settings.edit();
-        editor.putString(Integer.toString(urlSavedCount*2+1), url.getUrl());
-        editor.putString(Integer.toString(urlSavedCount*2+2), url.getMetadata());
+        editor.putString(Integer.toString(urlSavedCount * 2 + 1), url.getUrl());
+        editor.putString(Integer.toString(urlSavedCount * 2 + 2), url.getMetadata());
         editor.putInt("count", urlSavedCount + 1);
         editor.apply();
         Collections.sort(mList, new UrlComparator());
@@ -52,7 +54,7 @@ public class UrlList {
 
     public int removeUrl(Url url) {
         int pos = -1;
-        for(int i=0; i<mList.size(); i++) {
+        for (int i = 0; i < mList.size(); i++) {
             if (mList.elementAt(i).equals(url)) {
                 pos = i;
                 break;
@@ -82,9 +84,9 @@ public class UrlList {
     public void loadSavedUrls() {
         SharedPreferences settings = mContext.getSharedPreferences(PREFS_NAME, 0);
         int urlSavedCount = settings.getInt("count", 0);
-        for(int i = 0; i < urlSavedCount*2 ; i+=2) {
-            String url = settings.getString(Integer.toString(i+1), "");
-            String metadata = settings.getString(Integer.toString(i+2), "");
+        for (int i = 0; i < urlSavedCount * 2; i += 2) {
+            String url = settings.getString(Integer.toString(i + 1), "");
+            String metadata = settings.getString(Integer.toString(i + 2), "");
             if (url.isEmpty() || metadata.isEmpty()) {
                 continue;
             }
@@ -100,12 +102,12 @@ public class UrlList {
         SharedPreferences settings = mContext.getSharedPreferences(PREFS_NAME, 0);
         SharedPreferences.Editor editor = settings.edit();
         int urlSavedCount = settings.getInt("count", 0);
-        for(int i = 0; i < urlSavedCount*2 ; i+=2) {
-            String savedUrl = settings.getString(Integer.toString(i+1), "");
-            String savedMetadata = settings.getString(Integer.toString(i+2), "");
+        for (int i = 0; i < urlSavedCount * 2; i += 2) {
+            String savedUrl = settings.getString(Integer.toString(i + 1), "");
+            String savedMetadata = settings.getString(Integer.toString(i + 2), "");
             if (savedUrl.equals(url) && savedMetadata.equals(metadata)) {
-                editor.remove(Integer.toString(i+1));
-                editor.remove(Integer.toString(i+2));
+                editor.remove(Integer.toString(i + 1));
+                editor.remove(Integer.toString(i + 2));
             }
         }
         editor.apply();
