d70c1a7c8a05fe01145106dcdfd60e187f7b29fb
Ashish Bansal <bansal.ashish096@gmail.com>
2016-02-06 17:15:36 +0530

    Enabled enableAdvertisingIdCollection in Google Analytics

--- app/src/main/java/com/phantom/onetapvideodownload/AnalyticsApplication.java
+++ app/src/main/java/com/phantom/onetapvideodownload/AnalyticsApplication.java
@@ -38,6 +38,7 @@ public class AnalyticsApplication extends Application {
             GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
             // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
             mTracker = analytics.newTracker(R.xml.global_tracker);
+            mTracker.enableAdvertisingIdCollection(true);
         }
         return mTracker;
     }
