23b856fbbe19165030e7a6f8090137be00fb865e
Ashish Bansal <bansal.ashish096@gmail.com>
2016-04-05 12:43:20 +0530

    Added Google wallet as donation method

--- app/src/main/java/com/phantom/onetapvideodownload/ui/DonateActivity.java
+++ app/src/main/java/com/phantom/onetapvideodownload/ui/DonateActivity.java
@@ -1,16 +1,21 @@
 package com.phantom.onetapvideodownload.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.anjlab.android.iab.v3.BillingProcessor;
 import com.anjlab.android.iab.v3.TransactionDetails;
 import com.phantom.onetapvideodownload.R;
 import com.phantom.onetapvideodownload.utils.CheckPreferences;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DonateActivity extends AppCompatActivity implements BillingProcessor.IBillingHandler {
     private BillingProcessor mBillingProcessor;
@@ -34,10 +39,38 @@ public class DonateActivity extends AppCompatActivity implements BillingProcesso
         donateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean isAvailable = BillingProcessor.isIabServiceAvailable(DonateActivity.this);
-                if(isAvailable) {
-                    mBillingProcessor.purchase(DonateActivity.this, donationProductId);
-                }
+                final List<String> donationMethods = new ArrayList<>();
+                donationMethods.add(v.getContext().getString(R.string.via_wallet));
+                donationMethods.add(v.getContext().getString(R.string.via_playstore));
+
+                new MaterialDialog.Builder(v.getContext())
+                        .items(donationMethods)
+                        .itemsCallback(new MaterialDialog.ListCallback() {
+                            @Override
+                            public void onSelection(MaterialDialog dialog, View itemView, int which, CharSequence text) {
+                                switch(which) {
+                                    case 0 :
+                                        String url = "https://wallet.google.com/manage/#sendMoney:bansal.ashish096@gmail.com";
+                                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                                        intent.setData(Uri.parse(url));
+                                        startActivity(intent);
+                                        break;
+                                    case 1 :
+                                        boolean isAvailable = BillingProcessor.isIabServiceAvailable(DonateActivity.this);
+                                        if(isAvailable) {
+                                            mBillingProcessor.purchase(DonateActivity.this, donationProductId);
+                                        } else {
+                                            new MaterialDialog.Builder(dialog.getContext())
+                                                    .title(R.string.google_services_not_found_title)
+                                                    .content(R.string.google_services_not_found_summary)
+                                                    .positiveText(R.string.okay)
+                                                    .show();
+                                        }
+                                        break;
+                                }
+                            }
+                        })
+                        .show();
             }
         });
     }
